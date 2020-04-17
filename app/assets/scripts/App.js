01947668468e5d1267894c40dbbbad31e0e9a264
avidrutham <divyamathur89@gmail.com>
2020-04-17 15:10:06 +0530

    added html for resume section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,8 @@
 import "../styles/styles.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 import MobileMenu from "./modules/MobileMenu";
 import SlideShow from "./modules/SlideShow";
 import FixedNav from "./modules/FixedNav";
