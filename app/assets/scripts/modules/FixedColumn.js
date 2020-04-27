2c0caede0ac54b91f13d051e53a13dd77dd58cc5
avidrutham <divyamathur89@gmail.com>
2020-04-27 14:17:50 +0530

    add updateOnresize func to fixedcolumn js

--- app/assets/scripts/modules/FixedColumn.js
+++ app/assets/scripts/modules/FixedColumn.js
@@ -1,9 +1,3 @@
-const images = {
-  about: "assets/images/about.jpg",
-  resume: "assets/images/resume.jpg",
-  skills: "assets/images/game-keyboard.jpg"
-};
-
 class FixedColumn {
   constructor() {
     this.navlist = document.querySelector(".fixed-nav__list");
@@ -11,12 +5,27 @@ class FixedColumn {
     this.fixedColumnText = document.querySelector(
       ".fixed-column__text-content"
     );
+    this.images = {
+      about: "assets/images/about.jpg",
+      resume: "assets/images/resume.jpg",
+      skills: "assets/images/game-keyboard.jpg"
+    };
+    this.mediaQuery = window.matchMedia("(min-width: 1100px)");
     this.events();
   }
   events() {
+    this.setBanner("about");
     document.addEventListener("gumshoeActivate", event =>
       this.updateFixedColumn(event)
     );
+    window.addEventListener("resize", () => this.updateOnresize());
+  }
+
+  updateOnresize() {
+    let activeLink = document.querySelector(".fixed-nav__highlight");
+    if (this.mediaQuery.matches) {
+      this.setBanner(activeLink.getAttribute("data-link"));
+    }
   }
 
   updateFixedColumn(event) {
@@ -25,9 +34,11 @@ class FixedColumn {
   }
 
   setBanner(attribute) {
-    let imageLink = images[attribute];
-    this.fixedColumn.style.backgroundImage = `url('${imageLink}')`;
-    this.fixedColumnText.textContent = attribute;
+    if (this.mediaQuery.matches) {
+      let imageLink = this.images[attribute];
+      this.fixedColumn.style.backgroundImage = `url('${imageLink}')`;
+      this.fixedColumnText.textContent = attribute;
+    }
   }
 }
 
